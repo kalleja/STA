9400f810ecb26a8250599840119fc071caac6426
kalleja <kalle.jansson@metropolia.fi>
2020-05-08 01:51:05 +0300

    Update config_dev.js

--- server/config/config_dev.js
+++ server/config/config_dev.js
@@ -5,7 +5,7 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.set('useNewUrlParser', true);
 
 module.exports = {
-    MONGO_URI: "mongodb+srv://dbUser:mZQG1lCq2uEniLNN@cluster0-f2fqy.azure.mongodb.net/react-redux-websockets-passport?retryWrites=true&w=majority",
+    MONGO_URI: "mongodb+srv://dbUser:RCOD9nAySuIAppVw@cluster0-mmdib.mongodb.net/test?retryWrites=true&w=majority",
     JWT_SECRET: "jwt-react-redux-websockets-passport",
     JWT_TOKEN_EXPIRATION: 3600,
     SESSION_SECRET: "session-react-redux-websockets-passport",
